6efa8605a008d65b8c2506e221f82d55d81eebbf
zcz <1427364976@qq.com>
2021-06-07 20:12:31 +0800

    添加一个测试类

---
+++ src/test/java/com/baizhi/test.java
@@ -0,0 +1,12 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class test {
+    @Test
+    public void test(){
+        System.out.println("1111");
+    }
+}

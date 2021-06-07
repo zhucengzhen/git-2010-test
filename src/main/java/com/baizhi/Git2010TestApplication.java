95f35b9e36158c546a42605378828ff84586a900
zcz <1427364976@qq.com>
2021-06-07 19:40:04 +0800

    项目基础环境搭建完成

---
+++ src/main/java/com/baizhi/Git2010TestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2010TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2010TestApplication.class, args);
+    }
+
+}
